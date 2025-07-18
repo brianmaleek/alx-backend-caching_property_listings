7b92ea796e16587700ed05cb7f6997cfb8c5e48b
Brian Maleek <maleekmalik@gmail.com>
2025-07-19 02:19:51 +0300

    Add property list view with caching and JSON response in properties app

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .models import Property
 
 # Create your views here.
+@cache_page(60 * 15) #cache for 15 minutes (60 seconds * 15 minutes)
+def property_list(request):
+    """
+    View to list all properties
+
+    Args:
+        request (HttpRequest): The HTTP request object
+
+    Returns:
+        _type_: _description_
+    """
+    properties = Property.objects.all()
+    
+    #convert the properties to a list of dictionaries
+    property_data = []
+    for property in properties:
+        property_data.append({
+            'id': property.id,
+            'title': property.title,
+            'description': property.description,
+            'price': str(property.price),
+            'location': property.location,
+            'created_at': property.created_at.isoformat(),
+        })
+    return JsonResponse({
+        'properties': property_data,
+        'count': len(property_data)
+    })
