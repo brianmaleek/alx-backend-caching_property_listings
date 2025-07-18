d047d9b312717b918e72e70b9cffa62def84d372
Brian Maleek <maleekmalik@gmail.com>
2025-07-19 02:19:36 +0300

    Add PropertyAdmin class to Django admin for managing Property model with list display, search fields, and filters

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Property
 
 # Register your models here.
+admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'location', 'created_at')
+    search_fields = ('title', 'description', 'location')
+    list_filter = ('location', 'created_at')
+    ordering = ('-created_at',)
