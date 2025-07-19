7aea8ca00b75008050d9afca287454b767109cf8
Brian Maleek <maleekmalik@gmail.com>
2025-07-19 04:03:30 +0300

    Add ready method to PropertiesConfig for application initialization and signal registration

--- properties/apps.py
+++ properties/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "properties"
+
+    def ready(self):
+        """
+        This method is called when the application is ready.
+        It can be used to perform application initialization tasks.
+        """
+        # Import signals to ensure they are registered
+        import properties.signals  # Ensure signals are imported to register them
+        print("Properties app is ready and signals are imported.")    
