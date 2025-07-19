0273bf2f0d1a1eec76959fc5972bda2340821a81
Brian Maleek <maleekmalik@gmail.com>
2025-07-19 03:49:06 +0300

    Add cache invalidation on Property model changes using Django signals

--- properties/models.py
+++ properties/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from utils import invalidate_properties_cache
 
 # Create your models here.
-from django.db import models
-
 class Property(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -12,3 +13,16 @@ class Property(models.Model):
 
     def __str__(self):
         return self.title
+
+@receiver([post_save, post_delete], sender=Property)
+def invalidate_cache_on_property_change(sender, instance, **kwargs):
+    """
+    Signal receiver to invalidate the cache when a Property instance is saved or deleted.
+    
+    Args:
+        sender (Model): The model class that sent the signal.
+        instance (Property): The instance of the Property model.
+        **kwargs: Additional keyword arguments.
+    """
+    invalidate_properties_cache()
+    print(f"Cache invalidated for property: {instance.title} (ID: {instance.id})")
